7673388230cbe4daf4f5c21f9ee33abdc5ec7e46
RichVelo <richrar@gmail.com>
2022-07-29 12:11:44 +0100

    setup and basics

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!Doctype html>
+<html lang="en">
+<head>
+    <title>Rich Kerr | Dev.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <nav>
+
+    </nav>
+    <header>
+
+    </header>
+    <main>
+
+        <footer>
+            &copy Rich Kerr - bleep blorp...
+        </footer>
+    </main>
+</body>
+</html>
